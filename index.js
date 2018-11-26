4ba80054b54f1eee5608ece9feec1a917e298cbe
郭胜 <wb-gs465826@antfin.com>
2018-11-26 18:32:01 +0800

    【Hexagon】初步动画实现

--- index.js
+++ index.js
@@ -35,23 +35,122 @@ window['$Hexagon'] = class $Hexagon {
             centerX: el.width / 2,
             centerY: el.height / 2,
             X: el.width,
-            Y: el.height
+            Y: el.height,
+            line_l: 20, // 直线长度
+            angle: '60', // 角度
+            pos: { // 角度
+                '60': {
+                    sin: Math.sqrt(3) / 2,
+                    cos: 0.5
+                }
+            },
+            avg: 10, // now到next的执行次数
+            coor: { // 方向 - 坐标系四域
+                '1': e => ({ x: e.x + e.xl, y: e.y - e.yl, next: ['2', '1-4-x'] }),
+                '2': e => ({ x: e.x - e.xl, y: e.y - e.yl, next: ['1', '2-3-x'] }),
+                '3': e => ({ x: e.x - e.xl, y: e.y + e.yl, next: ['2-3-x', '4'] }),
+                '4': e => ({ x: e.x + e.xl, y: e.y + e.yl, next: ['1-4-x', '3'] }),
+                '1-4-x': e => ({ x: e.x + e.l, y: e.y, next: ['1', '4'] }),
+                '2-3-x': e => ({ x: e.x - e.l, y: e.y, next: ['2', '3'] })
+            }
         }
-        this.drawDotAnimation()
+        this.animation()
     }
-    drawDotAnimation () {
+    getEndXY ({x, y, next = ['1-4-x', '2-3-x']}) { // 求目标坐标
         const {
-            cxt,
-            centerX,
-            centerY
+            pos,
+            coor,
+            angle,
+            line_l
         } = this.data
-        cxt.strokeStyle = "#ffffff"
-        cxt.lineWidth = 3
-        cxt.lineCap = "round"
-        cxt.moveTo(centerX - 20, centerY)
-        cxt.lineTo(150, 50)
-        cxt.lineTo(10, 50)
+        return coor[next[parseInt(Math.random() * 2)]]({
+            x: x,
+            xl: line_l * pos[angle].cos,
+            y: y,
+            yl: line_l * pos[angle].sin,
+            l: line_l
+        })
+    }
+    getAvg (now, next) { // 计算等份
+        const {
+            avg
+        } = this.data
+        let pos = {
+            x: (next.x - now.x) / avg,
+            y: (next.y - now.y) / avg
+        }
+        let arr = []
+        arr.push({
+            i: 0,
+            ...now
+        })
+        for (let i = 0; i < avg - 1; i++) {
+            arr.push({
+                i: i + 1,
+                x: now.x + pos.x * (i + 1),
+                y: now.y + pos.y * (i + 1)
+            })
+        }
+        arr.push({
+            i: avg - 1,
+            ...next
+        })
+        return arr
+    }
+    drawLine (e) { // 画线
+        const {
+            cxt
+        } = this.data
+        const {
+            now,
+            next
+        } = e
+        let color = '#ffffff'
+        let line_w = 3
+        let line_c = 'round' // 直线圆润
+
+        cxt.strokeStyle = color
+        cxt.lineWidth = line_w
+        cxt.lineCap = line_c
+        cxt.moveTo(now.x, now.y) // 起始
+        cxt.lineTo(next.x, next.y)
         cxt.stroke()
+    }
+    animation () { // 动画
+        const {
+            centerX,
+            centerY,
+            avg,
+            line_l
+        } = this.data
+        let now = {
+            x: centerX - line_l * 2,
+            y: centerY
+        }
+        let next = this.getEndXY(now) // 取得next坐标
+        let avg_arr = this.getAvg(now, next) // 取得次数
+        let i = 0
+        // let raf = this.setInterval(() => {
+        //     this.drawLine({
+        //         now: now,
+        //         next: avg_arr[i]
+        //     })
+        //     if (avg_arr[i].i === avg -1) {
+        //         this.clearInterval(raf)
+        //     }
+        //     i++
+        // })
+        let fn = () => {
+            this.drawLine({
+                now: now,
+                next: avg_arr[i]
+            })
+            if (avg_arr[i].i !== avg - 1) {
+                window.requestAnimationFrame(fn)
+            }
+            i++
+        }
+        window.requestAnimationFrame(fn)
     }
     setInterval (fn) {
         let arr = this.data.raf_arr
